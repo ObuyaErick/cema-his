ec77611c0bf7fe21089ef75202913a6047539f6f
ObuyaErick <erickochieng766@gmail.com>
2025-04-24 14:03:47 +0300

    Vue masonry plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/ui', '@nuxt/fonts']
-})
+  runtimeConfig: {
+    apiSecret: "123",
+    public: {
+      apiBase: "/api",
+    },
+  },
+  modules: [
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxt/fonts",
+    "@vueuse/nuxt",
+  ],
+  css: ["~/assets/css/main.css"],
+  colorMode: {
+    preference: "system",
+    fallback: "light",
+  },
+});
